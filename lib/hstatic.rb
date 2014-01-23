7cf11801fc044cb807e86e1b8b97746c692397e6
Erick Pérez Castellanos <erick.red@gmail.com>
2014-01-23 01:59:53 -0500

    app: add proper -v swicth to cli tool
    code: fix and prettify

--- lib/hstatic.rb
+++ lib/hstatic.rb
@@ -7,9 +7,14 @@ module Hstatic
       if argv.any?
         require 'optparse'
         OptionParser.new { |op|
-          op.on('-p port',   'set the port (default is 4567)')                { |val| set :port, Integer(val) }
-          op.on('-s server', 'specify rack server/handler (default is thin)') { |val| set :server, val }
-          op.on('-x',        'turn on the mutex lock (default is off)')       {       set :lock, true }
+          op.on("-p port",   "--port port", "set the port (default is 4567)")                { |val| set :port, Integer(val) }
+          op.on("-s server",                "specify rack server/handler (default is thin)") { |val| set :server, val }
+          op.on("-x",                       "turn on the mutex lock (default is off)")       {       set :lock, true }
+
+          op.on("-v",        "--version",   "show version") do
+            puts "hstatic tool: v#{VERSION}"
+            exit
+          end
         }.parse!(ARGV.dup)
       end
 
