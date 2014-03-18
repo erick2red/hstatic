f6d51d16853466806380082ccb44253b8074f5ae
Erick Pérez Castellanos <erick.red@gmail.com>
2014-03-18 15:19:35 -0400

    doc: commented patching of String class

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require_relative '../lib/hstatic'
 require 'minitest/autorun'
 require "rack/test"
 
+# Migrated activesuport String#strip_heredoc patching
 class String
   def strip_heredoc
     indent = scan(/^[ \t]*(?=\S)/).min.size || 0
